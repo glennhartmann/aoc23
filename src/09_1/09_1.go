32ae1b23193b5e0616b362d3bf5fdd0357e0877a
Glenn Hartmann <hartmann.glenn@gmail.com>
2023-12-09 09:04:41 -0500

    Day 9

---
+++ src/09_1/09_1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+
+	"github.com/glennhartmann/aoc23/src/common/must"
+)
+
+func main() {
+	lines := must.GetFullInput()
+
+	sum := int64(0)
+	for i, line := range lines {
+		nums := must.ParseListOfNumbers64(line)
+		log.Printf("line %d: %v", i, nums)
+
+		diffs := make([][]int64, 0, 5)
+		diffs = append(diffs, nums)
+		for {
+			diffIndex := len(diffs) - 1
+
+			diff := make([]int64, 0, len(diffs[diffIndex]))
+
+			for i := 1; i < len(diffs[diffIndex]); i++ {
+				diff = append(diff, diffs[diffIndex][i]-diffs[diffIndex][i-1])
+			}
+
+			diffs = append(diffs, diff)
+			log.Printf("line %d: diffs[%d] = %v", i, diffIndex+1, diff)
+
+			if isAllZeroes(diff) {
+				break
+			}
+		}
+
+		diffs[len(diffs)-1] = append(diffs[len(diffs)-1], 0)
+		for j := len(diffs) - 2; j >= 0; j-- {
+			diffs[j] = append(diffs[j], diffs[j][len(diffs[j])-1]+diffs[j+1][len(diffs[j+1])-1])
+			log.Printf("line %d: expanded diffs[%d] = %v", i, j, diffs[j])
+		}
+
+		sum += diffs[0][len(diffs[0])-1]
+	}
+
+	log.Printf("final sum: %d", sum)
+}
+
+func isAllZeroes(nums []int64) bool {
+	for _, num := range nums {
+		if num != 0 {
+			return false
+		}
+	}
+	return true
+}
