glennhartmann/aoc23

commit 3911fb003779c8bcce17ad49163e1d7f14419400

Refactor some basic grid/map/maze-type utilities into a shared lib

src/10_1/10_1.go
@@ -4,38 +4,15 @@ import (
 	"log"
 
 	"github.com/glennhartmann/aoc23/src/common"
+	"github.com/glennhartmann/aoc23/src/common/grid/d4"
 	"github.com/glennhartmann/aoc23/src/common/must"
 )
-
-type direction int
-
-const (
-	up direction = iota
-	down
-	left
-	right
-)
-
-func (d direction) String() string {
-	switch d {
-	case up:
-		return "up"
-	case down:
-		return "down"
-	case left:
-		return "left"
-	case right:
-		return "right"
-	default:
-		panic("bad direction for stringer")
-	}
-}
 
 func main() {
 	lines := must.GetFullInput()
 	lines = common.AddSentinal(lines, ".")
 
-	x, y := findStart(lines)
+	x, y := d4.MustFindInStringGrid(lines, 'S')
 	s := determineStartShape(lines, x, y)
 	log.Printf("start [%d, %d] is %c", x, y, s)
 
@@ -43,28 +20,17 @@ func main() {
 	dirs := getDirections(s)
 	dir := dirs[0]
 	for {
-		x, y = move(x, y, dir)
+		x, y = d4.GetNextCell(x, y, dir)
 		log.Printf("moving %v to [%d, %d]", dir, x, y)
 
 		if lines[y][x] == 'S' {
 			break
 		}
 		nodesVisited++
-		dir = getOtherDir(lines[y][x], oppositeDir(dir))
+		dir = getOtherDir(lines[y][x], d4.OppositeDir(dir))
 	}
 	log.Printf("visited %d nodes", nodesVisited)
 	log.Printf("farthest distance: %d", nodesVisited/2)
-}
-
-func findStart(lines []string) (x, y int) {
-	for row := range lines {
-		for col := 0; col < len(lines[row]); col++ {
-			if lines[row][col] == 'S' {
-				return col, row
-			}
-		}
-	}
-	panic("start not found")
 }
 
 func determineStartShape(lines []string, x, y int) byte {
@@ -104,56 +70,26 @@ func connectsDown(p byte) bool {
 	return p == '|' || p == '7' || p == 'F'
 }
 
-func getDirections(p byte) []direction {
+func getDirections(p byte) []d4.Direction {
 	switch p {
 	case '|':
-		return []direction{up, down}
+		return []d4.Direction{d4.Up, d4.Down}
 	case '-':
-		return []direction{left, right}
+		return []d4.Direction{d4.Left, d4.Right}
 	case 'L':
-		return []direction{up, right}
+		return []d4.Direction{d4.Up, d4.Right}
 	case 'J':
-		return []direction{up, left}
+		return []d4.Direction{d4.Up, d4.Left}
 	case '7':
-		return []direction{down, left}
+		return []d4.Direction{d4.Down, d4.Left}
 	case 'F':
-		return []direction{down, right}
+		return []d4.Direction{d4.Down, d4.Right}
 	default:
 		panic("bad direction")
 	}
 }
 
-func move(x, y int, d direction) (xNew, yNew int) {
-	switch d {
-	case up:
-		return x, y - 1
-	case down:
-		return x, y + 1
-	case left:
-		return x - 1, y
-	case right:
-		return x + 1, y
-	default:
-		panic("bad direction for move")
-	}
-}
-
-func oppositeDir(dir direction) direction {
-	switch dir {
-	case up:
-		return down
-	case down:
-		return up
-	case left:
-		return right
-	case right:
-		return left
-	default:
-		panic("bad direction for opposite")
-	}
-}
-
-func getOtherDir(p byte, d direction) direction {
+func getOtherDir(p byte, d d4.Direction) d4.Direction {
 	dirs := getDirections(p)
 	if dirs[0] == d {
 		return dirs[1]

src/10_2/10_2.go
@@ -4,34 +4,11 @@ import (
 	"log"
 
 	"github.com/glennhartmann/aoc23/src/common"
+	"github.com/glennhartmann/aoc23/src/common/grid/d4"
 	"github.com/glennhartmann/aoc23/src/common/must"
 
 	c22 "github.com/glennhartmann/aoc22/src/common"
 )
-
-type direction int
-
-const (
-	up direction = iota
-	down
-	left
-	right
-)
-
-func (d direction) String() string {
-	switch d {
-	case up:
-		return "up"
-	case down:
-		return "down"
-	case left:
-		return "left"
-	case right:
-		return "right"
-	default:
-		panic("bad direction for stringer")
-	}
-}
 
 func main() {
 	lines := must.GetFullInput()
@@ -44,7 +21,7 @@ func main() {
 	lines = common.AddSentinal(lines, ".")
 	m = common.AddSentinal2(m, '.')
 
-	x, y := findStart(lines)
+	x, y := d4.MustFindInStringGrid(lines, 'S')
 	s := determineStartShape(lines, x, y)
 	log.Printf("start [%d, %d] is %c", x, y, s)
 
@@ -54,7 +31,7 @@ func main() {
 	dirs := getDirections(s)
 	dir := dirs[0]
 	for {
-		x, y = move(x, y, dir)
+		x, y = d4.GetNextCell(x, y, dir)
 		log.Printf("moving %v to [%d, %d]", dir, x, y)
 
 		if lines[y][x] == 'S' {
@@ -63,7 +40,7 @@ func main() {
 
 		m[y][x] = 'M' // part of Main loop
 
-		dir = getOtherDir(lines[y][x], oppositeDir(dir))
+		dir = getOtherDir(lines[y][x], d4.OppositeDir(dir))
 	}
 
 	log.Printf("secondary map main loop:\n%s", consolify(m))
@@ -71,7 +48,7 @@ func main() {
 	// categorize tiles adjacent to main loop as side 1 or side 2
 	// (we cheat and don't bother determining which side is 'in'
 	// and which is 'out')
-	x, y = findStart(lines)
+	x, y = d4.MustFindInStringGrid(lines, 'S')
 	dir = dirs[0]
 	side1Dir := getStartSide1Dir(s, dir)
 	adjacent := findAdjacentTilesInternal(lines, x, y, side1Dir, s)
@@ -87,14 +64,14 @@ func main() {
 			}
 		}
 
-		x, y = move(x, y, dir)
+		x, y = d4.GetNextCell(x, y, dir)
 
 		if lines[y][x] == 'S' {
 			break
 		}
 
 		oldDir := dir
-		dir = getOtherDir(lines[y][x], oppositeDir(dir))
+		dir = getOtherDir(lines[y][x], d4.OppositeDir(dir))
 		side1Dir = getSide1Dir(oldDir, side1Dir, lines[y][x])
 		adjacent = findAdjacentTiles(lines, x, y, side1Dir)
 	}
@@ -126,17 +103,6 @@ func main() {
 		}
 	}
 	log.Printf("final counts: side 1 (%d), side 2 (%d)", counts[0], counts[1])
-}
-
-func findStart(lines []string) (x, y int) {
-	for row := range lines {
-		for col := 0; col < len(lines[row]); col++ {
-			if lines[row][col] == 'S' {
-				return col, row
-			}
-		}
-	}
-	panic("start not found")
 }
 
 func determineStartShape(lines []string, x, y int) byte {
@@ -176,56 +142,26 @@ func connectsDown(p byte) bool {
 	return p == '|' || p == '7' || p == 'F'
 }
 
-func getDirections(p byte) []direction {
+func getDirections(p byte) []d4.Direction {
 	switch p {
 	case '|':
-		return []direction{up, down}
+		return []d4.Direction{d4.Up, d4.Down}
 	case '-':
-		return []direction{left, right}
+		return []d4.Direction{d4.Left, d4.Right}
 	case 'L':
-		return []direction{up, right}
+		return []d4.Direction{d4.Up, d4.Right}
 	case 'J':
-		return []direction{up, left}
+		return []d4.Direction{d4.Up, d4.Left}
 	case '7':
-		return []direction{down, left}
+		return []d4.Direction{d4.Down, d4.Left}
 	case 'F':
-		return []direction{down, right}
+		return []d4.Direction{d4.Down, d4.Right}
 	default:
 		panic("bad direction")
 	}
 }
 
-func move(x, y int, d direction) (xNew, yNew int) {
-	switch d {
-	case up:
-		return x, y - 1
-	case down:
-		return x, y + 1
-	case left:
-		return x - 1, y
-	case right:
-		return x + 1, y
-	default:
-		panic("bad direction for move")
-	}
-}
-
-func oppositeDir(dir direction) direction {
-	switch dir {
-	case up:
-		return down
-	case down:
-		return up
-	case left:
-		return right
-	case right:
-		return left
-	default:
-		panic("bad direction for opposite")
-	}
-}
-
-func getOtherDir(p byte, d direction) direction {
+func getOtherDir(p byte, d d4.Direction) d4.Direction {
 	dirs := getDirections(p)
 	if dirs[0] == d {
 		return dirs[1]
@@ -240,14 +176,14 @@ func consolify(m [][]byte) string {
 type adjacentTiles struct{ side1, side2 []point }
 type point struct{ x, y int }
 
-func findAdjacentTiles(lines []string, x, y int, d direction) adjacentTiles {
+func findAdjacentTiles(lines []string, x, y int, d d4.Direction) adjacentTiles {
 	return findAdjacentTilesInternal(lines, x, y, d, lines[y][x])
 }
 
-func findAdjacentTilesInternal(lines []string, x, y int, d direction, p byte) adjacentTiles {
+func findAdjacentTilesInternal(lines []string, x, y int, d d4.Direction, p byte) adjacentTiles {
 	adj := adjacentTiles{}
 	switch d {
-	case down:
+	case d4.Down:
 		switch p {
 		case '|':
 			break
@@ -294,7 +230,7 @@ func findAdjacentTilesInternal(lines []string, x, y int, d direction, p byte) ad
 		default:
 			panic("bad pipe in findAdjacentTiles down")
 		}
-	case up:
+	case d4.Up:
 		switch p {
 		case '|':
 			break
@@ -341,7 +277,7 @@ func findAdjacentTilesInternal(lines []string, x, y int, d direction, p byte) ad
 		default:
 			panic("bad pipe in findAdjacentTiles up")
 		}
-	case left:
+	case d4.Left:
 		switch p {
 		case '-':
 			break
@@ -388,7 +324,7 @@ func findAdjacentTilesInternal(lines []string, x, y int, d direction, p byte) ad
 		default:
 			panic("bad pipe in findAdjacentTiles left")
 		}
-	case right:
+	case d4.Right:
 		switch p {
 		case '-':
 			break
@@ -442,85 +378,85 @@ func findAdjacentTilesInternal(lines []string, x, y int, d direction, p byte) ad
 	return adj
 }
 
-func getSide1Dir(oldDir, oldSide1Dir direction, p byte) direction {
+func getSide1Dir(oldDir, oldSide1Dir d4.Direction, p byte) d4.Direction {
 	switch p {
 	case '-', '|':
 		return oldSide1Dir
 	case 'L':
-		if oldDir == down {
-			if oldSide1Dir == right {
-				return up
+		if oldDir == d4.Down {
+			if oldSide1Dir == d4.Right {
+				return d4.Up
 			}
-			return down
+			return d4.Down
 		}
-		if oldSide1Dir == up {
-			return right
+		if oldSide1Dir == d4.Up {
+			return d4.Right
 		}
-		return left
+		return d4.Left
 	case 'J':
-		if oldDir == down {
-			if oldSide1Dir == right {
-				return down
+		if oldDir == d4.Down {
+			if oldSide1Dir == d4.Right {
+				return d4.Down
 			}
-			return up
+			return d4.Up
 		}
-		if oldSide1Dir == up {
-			return left
+		if oldSide1Dir == d4.Up {
+			return d4.Left
 		}
-		return right
+		return d4.Right
 	case '7':
-		if oldDir == up {
-			if oldSide1Dir == right {
-				return up
+		if oldDir == d4.Up {
+			if oldSide1Dir == d4.Right {
+				return d4.Up
 			}
-			return down
+			return d4.Down
 		}
-		if oldSide1Dir == up {
-			return right
+		if oldSide1Dir == d4.Up {
+			return d4.Right
 		}
-		return left
+		return d4.Left
 	case 'F':
-		if oldDir == up {
-			if oldSide1Dir == right {
-				return down
+		if oldDir == d4.Up {
+			if oldSide1Dir == d4.Right {
+				return d4.Down
 			}
-			return up
+			return d4.Up
 		}
-		if oldSide1Dir == up {
-			return left
+		if oldSide1Dir == d4.Up {
+			return d4.Left
 		}
-		return right
+		return d4.Right
 	default:
 		panic("bad pipe in getSideDir")
 	}
 }
 
-func getStartSide1Dir(p byte, d direction) direction {
+func getStartSide1Dir(p byte, d d4.Direction) d4.Direction {
 	switch p {
 	case '|':
-		return left
+		return d4.Left
 	case '-':
-		return up
+		return d4.Up
 	case 'L':
-		if d == right {
-			return up
+		if d == d4.Right {
+			return d4.Up
 		}
-		return right
+		return d4.Right
 	case 'J':
-		if d == left {
-			return up
+		if d == d4.Left {
+			return d4.Up
 		}
-		return left
+		return d4.Left
 	case '7':
-		if d == left {
-			return down
+		if d == d4.Left {
+			return d4.Down
 		}
-		return left
+		return d4.Left
 	case 'F':
-		if d == right {
-			return down
+		if d == d4.Right {
+			return d4.Down
 		}
-		return right
+		return d4.Right
 	default:
 		panic("bad pipe in getStartSide1Dir")
 	}

src/16_1/16_1.go
@@ -6,40 +6,17 @@ import (
 	"strings"
 
 	"github.com/glennhartmann/aoc23/src/common"
+	"github.com/glennhartmann/aoc23/src/common/grid/d4"
 	"github.com/glennhartmann/aoc23/src/common/must"
 )
 
-type direction int
-
-const (
-	up direction = iota
-	down
-	left
-	right
-)
-
-func (dir direction) String() string {
-	switch dir {
-	case up:
-		return "up"
-	case down:
-		return "down"
-	case left:
-		return "left"
-	case right:
-		return "right"
-	default:
-		panic("bad direction")
-	}
-}
-
 type beam struct {
-	dir  direction
+	dir  d4.Direction
 	r, c int
 }
 
 type cell struct {
-	beams map[direction]struct{}
+	beams map[d4.Direction]struct{}
 }
 
 func main() {
@@ -47,13 +24,13 @@ func main() {
 	lines = common.AddSentinal(lines, ",")
 
 	beams := make([]*beam, 0, 10)
-	beams = append(beams, &beam{dir: right, r: 1, c: 1})
+	beams = append(beams, &beam{dir: d4.Right, r: 1, c: 1})
 
 	cells := make([][]cell, len(lines))
 	for r := range cells {
 		cells[r] = make([]cell, len(lines[0]))
 		for c := range cells[r] {
-			cells[r][c] = cell{beams: make(map[direction]struct{}, 4)}
+			cells[r][c] = cell{beams: make(map[d4.Direction]struct{}, 4)}
 		}
 	}
 	for b := 0; b < len(beams); b++ {
@@ -73,11 +50,11 @@ func main() {
 			nDir, nBeam := getNextDir(r, c, lines, dir)
 			beams[b].dir = nDir
 			log.Printf("beam %d: next dir: %v", b, nDir)
-			beams[b].r, beams[b].c = getNextCell(r, c, nDir)
+			beams[b].r, beams[b].c = d4.GetNextCell(r, c, nDir)
 			log.Printf("beam %d: next cell: {%d, %d}", b, beams[b].r, beams[b].c)
 
 			if nBeam != nil {
-				nBeam.r, nBeam.c = getNextCell(nBeam.r, nBeam.c, nBeam.dir)
+				nBeam.r, nBeam.c = d4.GetNextCell(nBeam.r, nBeam.c, nBeam.dir)
 				log.Printf("new beam! next cell: {%d, %d}", nBeam.r, nBeam.c)
 				beams = append(beams, nBeam)
 			}
@@ -93,13 +70,13 @@ func main() {
 			} else if len(cells[r][c].beams) == 1 {
 				if lines[r][c] == ',' {
 					fmt.Fprintf(&sb, ",")
-				} else if _, ok := cells[r][c].beams[up]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Up]; ok {
 					fmt.Fprintf(&sb, "^")
-				} else if _, ok := cells[r][c].beams[down]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Down]; ok {
 					fmt.Fprintf(&sb, "v")
-				} else if _, ok := cells[r][c].beams[left]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Left]; ok {
 					fmt.Fprintf(&sb, "<")
-				} else if _, ok := cells[r][c].beams[right]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Right]; ok {
 					fmt.Fprintf(&sb, ">")
 				}
 			} else if len(cells[r][c].beams) > 1 {
@@ -130,70 +107,55 @@ func main() {
 	log.Printf("total energized: %d", count)
 }
 
-func getNextDir(r, c int, lines []string, dir direction) (direction, *beam) {
+func getNextDir(r, c int, lines []string, dir d4.Direction) (d4.Direction, *beam) {
 	switch lines[r][c] {
 	case '.':
 		return dir, nil
 	case '/':
 		switch dir {
-		case up:
-			return right, nil
-		case down:
-			return left, nil
-		case left:
-			return down, nil
-		case right:
-			return up, nil
+		case d4.Up:
+			return d4.Right, nil
+		case d4.Down:
+			return d4.Left, nil
+		case d4.Left:
+			return d4.Down, nil
+		case d4.Right:
+			return d4.Up, nil
 		default:
 			panic("bad dir")
 		}
 	case '\\':
 		switch dir {
-		case up:
-			return left, nil
-		case down:
-			return right, nil
-		case left:
-			return up, nil
-		case right:
-			return down, nil
+		case d4.Up:
+			return d4.Left, nil
+		case d4.Down:
+			return d4.Right, nil
+		case d4.Left:
+			return d4.Up, nil
+		case d4.Right:
+			return d4.Down, nil
 		default:
 			panic("bad dir")
 		}
 	case '|':
 		switch dir {
-		case up, down:
+		case d4.Up, d4.Down:
 			return dir, nil
-		case left, right:
-			return down, &beam{dir: up, r: r, c: c}
+		case d4.Left, d4.Right:
+			return d4.Down, &beam{dir: d4.Up, r: r, c: c}
 		default:
 			panic("bad dir")
 		}
 	case '-':
 		switch dir {
-		case up, down:
-			return left, &beam{dir: right, r: r, c: c}
-		case left, right:
+		case d4.Up, d4.Down:
+			return d4.Left, &beam{dir: d4.Right, r: r, c: c}
+		case d4.Left, d4.Right:
 			return dir, nil
 		default:
 			panic("bad dir")
 		}
 	default:
 		panic("bad cell")
-	}
-}
-
-func getNextCell(r, c int, dir direction) (nx, ny int) {
-	switch dir {
-	case up:
-		return r - 1, c
-	case down:
-		return r + 1, c
-	case left:
-		return r, c - 1
-	case right:
-		return r, c + 1
-	default:
-		panic("bad dir")
 	}
 }

src/16_2/16_2.go
@@ -6,40 +6,17 @@ import (
 	"strings"
 
 	"github.com/glennhartmann/aoc23/src/common"
+	"github.com/glennhartmann/aoc23/src/common/grid/d4"
 	"github.com/glennhartmann/aoc23/src/common/must"
 )
 
-type direction int
-
-const (
-	up direction = iota
-	down
-	left
-	right
-)
-
-func (dir direction) String() string {
-	switch dir {
-	case up:
-		return "up"
-	case down:
-		return "down"
-	case left:
-		return "left"
-	case right:
-		return "right"
-	default:
-		panic("bad direction")
-	}
-}
-
 type beam struct {
-	dir  direction
+	dir  d4.Direction
 	r, c int
 }
 
 type cell struct {
-	beams map[direction]struct{}
+	beams map[d4.Direction]struct{}
 }
 
 func main() {
@@ -49,32 +26,32 @@ func main() {
 	energized := -1
 	for r := 1; r < len(lines)-1; r++ {
 		if r == 1 {
-			energized = max(energized, oldMain(lines, r, 1, down))
-			energized = max(energized, oldMain(lines, r, len(lines)-2, down))
+			energized = max(energized, oldMain(lines, r, 1, d4.Down))
+			energized = max(energized, oldMain(lines, r, len(lines)-2, d4.Down))
 		}
 		if r == len(lines)-2 {
-			energized = max(energized, oldMain(lines, r, 1, up))
-			energized = max(energized, oldMain(lines, r, len(lines)-2, up))
+			energized = max(energized, oldMain(lines, r, 1, d4.Up))
+			energized = max(energized, oldMain(lines, r, len(lines)-2, d4.Up))
 		}
-		energized = max(energized, oldMain(lines, r, 1, right))
-		energized = max(energized, oldMain(lines, r, len(lines)-2, left))
+		energized = max(energized, oldMain(lines, r, 1, d4.Right))
+		energized = max(energized, oldMain(lines, r, len(lines)-2, d4.Left))
 	}
 	for c := 1; c < len(lines[0])-1; c++ {
 		if c == 1 {
-			energized = max(energized, oldMain(lines, 1, c, right))
-			energized = max(energized, oldMain(lines, len(lines[0])-2, c, right))
+			energized = max(energized, oldMain(lines, 1, c, d4.Right))
+			energized = max(energized, oldMain(lines, len(lines[0])-2, c, d4.Right))
 		}
 		if c == len(lines[0])-2 {
-			energized = max(energized, oldMain(lines, 1, c, left))
-			energized = max(energized, oldMain(lines, len(lines[0])-2, c, left))
+			energized = max(energized, oldMain(lines, 1, c, d4.Left))
+			energized = max(energized, oldMain(lines, len(lines[0])-2, c, d4.Left))
 		}
-		energized = max(energized, oldMain(lines, 1, c, down))
-		energized = max(energized, oldMain(lines, len(lines[0])-2, c, up))
+		energized = max(energized, oldMain(lines, 1, c, d4.Down))
+		energized = max(energized, oldMain(lines, len(lines[0])-2, c, d4.Up))
 	}
 	log.Printf("max energized: %d", energized)
 }
 
-func oldMain(lines []string, startR, startC int, startDir direction) int {
+func oldMain(lines []string, startR, startC int, startDir d4.Direction) int {
 	beams := make([]*beam, 0, 10)
 	beams = append(beams, &beam{dir: startDir, r: startR, c: startC})
 
@@ -82,7 +59,7 @@ func oldMain(lines []string, startR, startC int, startDir direction) int {
 	for r := range cells {
 		cells[r] = make([]cell, len(lines[0]))
 		for c := range cells[r] {
-			cells[r][c] = cell{beams: make(map[direction]struct{}, 4)}
+			cells[r][c] = cell{beams: make(map[d4.Direction]struct{}, 4)}
 		}
 	}
 	for b := 0; b < len(beams); b++ {
@@ -102,11 +79,11 @@ func oldMain(lines []string, startR, startC int, startDir direction) int {
 			nDir, nBeam := getNextDir(r, c, lines, dir)
 			beams[b].dir = nDir
 			log.Printf("beam %d: next dir: %v", b, nDir)
-			beams[b].r, beams[b].c = getNextCell(r, c, nDir)
+			beams[b].r, beams[b].c = d4.GetNextCell(r, c, nDir)
 			log.Printf("beam %d: next cell: {%d, %d}", b, beams[b].r, beams[b].c)
 
 			if nBeam != nil {
-				nBeam.r, nBeam.c = getNextCell(nBeam.r, nBeam.c, nBeam.dir)
+				nBeam.r, nBeam.c = d4.GetNextCell(nBeam.r, nBeam.c, nBeam.dir)
 				log.Printf("new beam! next cell: {%d, %d}", nBeam.r, nBeam.c)
 				beams = append(beams, nBeam)
 			}
@@ -122,13 +99,13 @@ func oldMain(lines []string, startR, startC int, startDir direction) int {
 			} else if len(cells[r][c].beams) == 1 {
 				if lines[r][c] == ',' {
 					fmt.Fprintf(&sb, ",")
-				} else if _, ok := cells[r][c].beams[up]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Up]; ok {
 					fmt.Fprintf(&sb, "^")
-				} else if _, ok := cells[r][c].beams[down]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Down]; ok {
 					fmt.Fprintf(&sb, "v")
-				} else if _, ok := cells[r][c].beams[left]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Left]; ok {
 					fmt.Fprintf(&sb, "<")
-				} else if _, ok := cells[r][c].beams[right]; ok {
+				} else if _, ok := cells[r][c].beams[d4.Right]; ok {
 					fmt.Fprintf(&sb, ">")
 				}
 			} else if len(cells[r][c].beams) > 1 {
@@ -160,70 +137,55 @@ func oldMain(lines []string, startR, startC int, startDir direction) int {
 	return count
 }
 
-func getNextDir(r, c int, lines []string, dir direction) (direction, *beam) {
+func getNextDir(r, c int, lines []string, dir d4.Direction) (d4.Direction, *beam) {
 	switch lines[r][c] {
 	case '.':
 		return dir, nil
 	case '/':
 		switch dir {
-		case up:
-			return right, nil
-		case down:
-			return left, nil
-		case left:
-			return down, nil
-		case right:
-			return up, nil
+		case d4.Up:
+			return d4.Right, nil
+		case d4.Down:
+			return d4.Left, nil
+		case d4.Left:
+			return d4.Down, nil
+		case d4.Right:
+			return d4.Up, nil
 		default:
 			panic("bad dir")
 		}
 	case '\\':
 		switch dir {
-		case up:
-			return left, nil
-		case down:
-			return right, nil
-		case left:
-			return up, nil
-		case right:
-			return down, nil
+		case d4.Up:
+			return d4.Left, nil
+		case d4.Down:
+			return d4.Right, nil
+		case d4.Left:
+			return d4.Up, nil
+		case d4.Right:
+			return d4.Down, nil
 		default:
 			panic("bad dir")
 		}
 	case '|':
 		switch dir {
-		case up, down:
+		case d4.Up, d4.Down:
 			return dir, nil
-		case left, right:
-			return down, &beam{dir: up, r: r, c: c}
+		case d4.Left, d4.Right:
+			return d4.Down, &beam{dir: d4.Up, r: r, c: c}
 		default:
 			panic("bad dir")
 		}
 	case '-':
 		switch dir {
-		case up, down:
-			return left, &beam{dir: right, r: r, c: c}
-		case left, right:
+		case d4.Up, d4.Down:
+			return d4.Left, &beam{dir: d4.Right, r: r, c: c}
+		case d4.Left, d4.Right:
 			return dir, nil
 		default:
 			panic("bad dir")
 		}
 	default:
 		panic("bad cell")
-	}
-}
-
-func getNextCell(r, c int, dir direction) (nx, ny int) {
-	switch dir {
-	case up:
-		return r - 1, c
-	case down:
-		return r + 1, c
-	case left:
-		return r, c - 1
-	case right:
-		return r, c + 1
-	default:
-		panic("bad dir")
 	}
 }

src/common/grid/d4/d4.go
@@ -1,0 +1,47 @@
+package d4
+
+import (
+	"github.com/glennhartmann/aoc23/src/common"
+	"github.com/glennhartmann/aoc23/src/common/grid/d8"
+)
+
+type Direction int
+
+const (
+	Up    = Direction(d8.Up)
+	Down  = Direction(d8.Down)
+	Left  = Direction(d8.Left)
+	Right = Direction(d8.Right)
+)
+
+func (dir Direction) String() string {
+	return d8.Direction(dir).String()
+}
+
+func GetDirChar(dir Direction) byte {
+	switch dir {
+	case Up:
+		return '^'
+	case Down:
+		return 'v'
+	case Left:
+		return '<'
+	case Right:
+		return '>'
+	default:
+		common.Panicf("invalid direction: %v", dir)
+	}
+	return '!'
+}
+
+func GetNextCell(r, c int, dir Direction) (nr, nc int) {
+	return d8.GetNextCell(r, c, d8.Direction(dir))
+}
+
+func OppositeDir(dir Direction) Direction {
+	return Direction(d8.OppositeDir(d8.Direction(dir)))
+}
+
+func MustFindInStringGrid(lines []string, c byte) (x, y int) {
+	return d8.MustFindInStringGrid(lines, c)
+}

src/common/grid/d8/d8.go
@@ -1,0 +1,100 @@
+package d8
+
+import "github.com/glennhartmann/aoc23/src/common"
+
+type Direction int
+
+const (
+	Up Direction = iota
+	Down
+	Left
+	Right
+	UpLeft
+	UpRight
+	DownLeft
+	DownRight
+)
+
+func (dir Direction) String() string {
+	switch dir {
+	case Up:
+		return "up"
+	case Down:
+		return "down"
+	case Left:
+		return "left"
+	case Right:
+		return "right"
+	case UpLeft:
+		return "up-left"
+	case UpRight:
+		return "up-right"
+	case DownLeft:
+		return "down-left"
+	case DownRight:
+		return "down-right"
+	default:
+		common.Panicf("invalid direction: %v", dir)
+	}
+	return "INVALID"
+}
+
+func GetNextCell(r, c int, dir Direction) (nr, nc int) {
+	switch dir {
+	case Up:
+		return r - 1, c
+	case Down:
+		return r + 1, c
+	case Left:
+		return r, c - 1
+	case Right:
+		return r, c + 1
+	case UpLeft:
+		return r - 1, c + 1
+	case UpRight:
+		return r - 1, c + 1
+	case DownLeft:
+		return r + 1, c - 1
+	case DownRight:
+		return r + 1, c + 1
+	default:
+		common.Panicf("invalid direction: %v", dir)
+	}
+	return -1, -1
+}
+
+func OppositeDir(dir Direction) Direction {
+	switch dir {
+	case Up:
+		return Down
+	case Down:
+		return Up
+	case Left:
+		return Right
+	case Right:
+		return Left
+	case UpLeft:
+		return DownRight
+	case UpRight:
+		return DownLeft
+	case DownLeft:
+		return UpRight
+	case DownRight:
+		return UpLeft
+	default:
+		common.Panicf("invalid direction: %v", dir)
+	}
+	return Direction(-1)
+}
+
+func MustFindInStringGrid(lines []string, c byte) (x, y int) {
+	for row := range lines {
+		for col := 0; col < len(lines[row]); col++ {
+			if lines[row][col] == c {
+				return col, row
+			}
+		}
+	}
+	common.Panicf("%c not found", c)
+	return -1, -1
+}

commit 51e4fb94fcd290acf1de9cb3681fae8010060398

Fix day 10 solution
It was broken by
https://github.com/glennhartmann/aoc23/commit/3911fb003779c8bcce17ad49163e1d7f14419400

src/10_1/10_1.go
@@ -20,7 +20,7 @@ func main() {
 	dirs := getDirections(s)
 	dir := dirs[0]
 	for {
-		x, y = d4.GetNextCell(x, y, dir)
+		y, x = d4.GetNextCell(y, x, dir)
 		log.Printf("moving %v to [%d, %d]", dir, x, y)
 
 		if lines[y][x] == 'S' {

src/10_2/10_2.go
@@ -31,7 +31,7 @@ func main() {
 	dirs := getDirections(s)
 	dir := dirs[0]
 	for {
-		x, y = d4.GetNextCell(x, y, dir)
+		y, x = d4.GetNextCell(y, x, dir)
 		log.Printf("moving %v to [%d, %d]", dir, x, y)
 
 		if lines[y][x] == 'S' {
@@ -64,7 +64,7 @@ func main() {
 			}
 		}
 
-		x, y = d4.GetNextCell(x, y, dir)
+		y, x = d4.GetNextCell(y, x, dir)
 
 		if lines[y][x] == 'S' {
 			break